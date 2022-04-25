780233a55d1933643e4b2e92363b149ae576cf58
Vaishali Singh <vaishali.s@celestialsys.com>
2022-04-25 17:48:42 +0530

    push 5

--- TestScript/totestfile.py
+++ TestScript/totestfile.py
@@ -5,8 +5,6 @@ import time
 
 time.sleep(60)
 print(f"Printing a randome number{random.random()}")
-print(f"Printing a randome number{random.random()}")
-print(f"Printing a randome number{random.random()}")
 print("Lets test")
 print("one more user added")
 print("one more user added")
